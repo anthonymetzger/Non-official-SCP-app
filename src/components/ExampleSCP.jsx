737d7b755b0dabde9604e24963b49d0849ee556f
AnthonyMetzger <tony.m.metzger@gmail.com>
2026-01-22 13:19:39 -0500

    First commit

---
+++ src/components/ExampleSCP.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+import image from '../assets/SCP173Example.webp';
+
+function ExampleSCP() {
+    return (
+        <>
+            <div>
+                <h2>Example SCP Component SCP NAME HERE</h2>
+                <img src={image} alt="Example SCP Image" />
+                <p>This is an example SCP component SPC DESCRIPTION HERE</p>
+            </div>
+        </>
+    )
+}
+
+export default ExampleSCP;
---
+++ src/hooks/exampleAPI.jsx
@@ -0,0 +1,14 @@
+//I am going to try to see how to get data from this example API hook to the main window component.
+import react from 'react';
+
+function useExampleAPI() {
+    //A am trying to get 
+
+    let dataTest = "Hello from Example API Hook";
+
+    return dataTest;
+}
+//and finally the export
+
+export default useExampleAPI;
+
